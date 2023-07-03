e51d8280388989de26af1ec7a05dbd921ed24627
Luke Rollans <luke@rollans.sh>
2023-07-03 22:53:02 +0800

    us-east-2!

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -68,7 +68,7 @@ export FARGATE_CPU_SIZE=${FARGATE_CPU_SIZE:-512}
 export FARGATE_MEMORY_SIZE=${FARGATE_MEMORY_SIZE:-1024}
 
 # Set AWS_DEFAULT_REGION as per the supplied Perx region
-export AWS_DEFAULT_REGION=$([ $INPUT_PERX_REGION == "au" ] && echo "ap-southeast-2" || echo "us-east-1") 
+export AWS_DEFAULT_REGION=$([ $INPUT_PERX_REGION == "au" ] && echo "ap-southeast-2" || echo "us-east-2") 
 
 success "Optional environment variables OK"
 
